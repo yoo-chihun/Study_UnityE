9e055464457182476775a19c0c6ed9fe09e54bb0
rkdghkclgns-design <rkdghkclgns@naver.com>
2025-11-26 16:55:15 +0900

    251126_최종 커밋
    .

---
+++ Project_E/Assets/Script/Study.cs
@@ -0,0 +1,17 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Study : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+
+    void Update()
+    {
+        
+    }
+}
+
+
